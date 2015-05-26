f6bc9f2d42ce258c09ccde05ad127308c2f6217b
Oliver Stenkilde <oliver@stenkilde.com>
2015-05-26 08:46:40 +0200

    added it to git

---
+++ app/modules/timeline/timeline.controller.js
@@ -0,0 +1,30 @@
+(function () {
+	'use strict';
+
+	/**
+	 * @ngdoc function
+	 * @name timelineApp.controller:TimelineCtrl
+	 * @description
+	 * # TimelineCtrl
+	 * Controller of the timelineApp
+	 */
+	angular
+		.module('timelineApp')
+		.controller('Timeline', Timeline);
+
+	/* @ngInject */
+	function Timeline($http, $scope) {
+		/*jshint validthis: true */
+		var vm = this;
+
+		activate();
+
+		function activate() {
+			$http.get('timeline.json')
+				.success(function(response) {
+					$scope.names = response;
+				});
+		}
+	};
+
+})();
---
+++ app/modules/timeline/timeline.route.js
@@ -0,0 +1,26 @@
+(function() {
+  'use strict';
+
+  /**
+   * @ngdoc route
+   * @name timelineApp.route:timeline
+   * @function
+   * @description
+   * # timeline
+   * Route in the timelineApp.
+   */
+  angular.module('timelineApp')
+    /* @ngInject */
+    .config(function ($stateProvider) {
+
+      var Timeline = {
+        name: 'timeline',
+        url: '/',
+        templateUrl: 'modules/timeline/timeline.template.html',
+        controller: 'Timeline',
+        controllerAs: 'timeline'
+      };
+
+      $stateProvider.state(Timeline);
+    });
+})();
